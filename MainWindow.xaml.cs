90f789c72e904682723ffd4b29b2b114501d14f5
MalikBogos <malikjandarov@gmail.com>
2024-10-16 13:24:33 +0200

    Adding numbered rows, scrolling, edited UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,45 +1,169 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Controls;
+using System.Data;
 
 namespace FuturePortfolio
 {
     public partial class MainWindow : Window
     {
+        private SpreadsheetData _data;
+
         public MainWindow()
         {
             InitializeComponent();
-            VoegKolommenRijenToe();
+            _data = new SpreadsheetData(100, 3);
+            ExcelLikeGrid.ItemsSource = _data;
+
+            ExcelLikeGrid.LoadingRow += ExcelLikeGrid_LoadingRow;
         }
 
-        private void VoegKolommenRijenToe()
+        private void ExcelLikeGrid_LoadingRow(object sender, DataGridRowEventArgs e)
         {
-            List<CellData> data = new List<CellData>
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+
+        public void SaveSpreadsheet(string filePath)
+        {
+            var jsonString = JsonSerializer.Serialize(_data);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public void LoadSpreadsheet(string filePath)
+        {
+            var jsonString = File.ReadAllText(filePath);
+            _data = JsonSerializer.Deserialize<SpreadsheetData>(jsonString);
+            ExcelLikeGrid.ItemsSource = _data;
+        }
+
+        public void ApplyFormatToCell(int row, int column, CellFormat format)
+        {
+            var cell = _data.GetCell(row, column);
+            cell.Format = format;
+        }
+
+        private void ExcelLikeGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            if (e.EditAction == DataGridEditAction.Commit)
             {
-                new CellData { Kolom1 = "Data 1", Kolom2 = "Data 2", Kolom3 = "Data 3" },
-                new CellData { Kolom1 = "Data 4", Kolom2 = "Data 5", Kolom3 = "Data 6" },
-                new CellData { Kolom1 = "Data 7", Kolom2 = "Data 8", Kolom3 = "Data 9" },
-                new CellData { Kolom1 = "", Kolom2 = "", Kolom3 = "", Kolom4 = "Hello"}
+                var cell = (e.Row.Item as ObservableCollection<Cell>)[e.Column.DisplayIndex];
+                var editedValue = (e.EditingElement as TextBox).Text;
 
-            };
-
-            SpreadsheetGrid.ItemsSource = data;
+                if (editedValue.StartsWith("="))
+                {
+                    cell.Formula = editedValue;
+                    cell.Value = FormulaCalculator.CalculateFormula(editedValue);
+                }
+                else
+                {
+                    cell.Value = editedValue;
+                    cell.Formula = null;
+                }
+            }
         }
     }
- 
-    public class CellData
+}
+
+public class Cell : INotifyPropertyChanged
+{
+    private string _value;
+    private string _formula;
+    private CellFormat _format;
+
+    public string Value
     {
-        public string Kolom1 { get; set; }
-        public string Kolom2 { get; set; }
-        public string Kolom3 { get; set; }
-        public string Kolom4 { get; set; }
+        get => _value;
+        set
+        {
+            _value = value;
+            OnPropertyChanged(nameof(Value));
+        }
     }
 
+    public string Formula
+    {
+        get => _formula;
+        set
+        {
+            _formula = value;
+            OnPropertyChanged(nameof(Formula));
+        }
+    }
+
+    public CellFormat Format
+    {
+        get => _format;
+        set
+        {
+            _format = value;
+            OnPropertyChanged(nameof(Format));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+public class CellFormat
+{
+    public FontStyle FontStyle { get; set; }
+    public FontWeight FontWeight { get; set; }
+    public Color ForegroundColor { get; set; }
+    public Color BackgroundColor { get; set; }
+}
+
+public class SpreadsheetData : ObservableCollection<ObservableCollection<Cell>>
+{
+    public SpreadsheetData(int rows, int columns)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            var row = new ObservableCollection<Cell>();
+            for (int j = 0; j < columns; j++)
+            {
+                row.Add(new Cell());
+            }
+            Add(row);
+        }
+    }
+
+    public Cell GetCell(int row, int column)
+    {
+        return this[row][column];
+    }
+
+    public void SetCellValue(int row, int column, string value)
+    {
+        this[row][column].Value = value;
+    }
+}
+public static class FormulaCalculator
+{
+    public static string CalculateFormula(string formula)
+    {
+        if (string.IsNullOrWhiteSpace(formula) || !formula.StartsWith("="))
+            return formula;
+
+        formula = formula.Substring(1);
+
+        try
+        {
+            DataTable dt = new DataTable();
+            var result = dt.Compute(formula, "");
+            return result.ToString();
+        }
+        catch (Exception ex)
+        {
+            return $"Error: {ex.Message}";
+        }
+    }
 }
